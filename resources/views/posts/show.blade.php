eeeb9f072b2f3baa0e1bfc2e166ce8d3c59a15a3
clnr073 <nengfan073@gmail.com>
2023-10-24 16:02:19 +0900

    Create blog post creation screen

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,7 +7,7 @@
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     </head>
-    <body>
+    <body class="antialiased">
         <h1 class="title">
             {{ $post->title }}
         </h1>
