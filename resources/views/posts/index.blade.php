9b0765c4a1c6da28d5cc4bf029ef2b2e2286d370
clnr073 <nengfan073@gmail.com>
2023-10-28 11:32:02 +0900

    Implementation of softdeleting blog posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -17,11 +17,27 @@
                         <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
                     </h2>
                     <p class='body'>{{ $post->body }}</p>
+                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" onclick="deletePost({{ $post->id }})">delete</button> <!--onclickでJavaScriptのdeletePost関数を実行-->
+                    </form>
                 </div>
             @endforeach
         </div>
         <div class='paginate'>
             {{ $posts->links() }}
         </div>
+        <script>
+            function deletePost(id) {
+                'use strict'
+                
+                if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                    document.getElementById(`form_${id}`).submit();
+                    // getElementByIdの引数にidを設定し、設定したidの要素を取得
+                    // 引数にform_がついているのは、投稿ごとに区別するため
+                }
+            }
+        </script>
     </body>
 </html>
