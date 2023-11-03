08e8ea4756412c4747bfdbec69af76bf558070c6
clnr073 <nengfan073@gmail.com>
2023-11-03 16:33:05 +0900

    Defined a relationship and made it possible to select a category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    
+    // Postに対するリレーション
+    
+    // 「1対多」の関係なので'posts'と複数形に
+    public function posts()
+    {
+        return $this->hasMany(post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,12 +14,21 @@ class Post extends Model
     // fillが可能なプロパティを指定する
     protected $fillable = [
         'title',
-        'body'
+        'body',
+        'category_id'
         ];
     
     public function getPaginateByLimit(int $limit_count = 5)
     {
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
         // updated_atカラムの降順で並び替え　// ページネーションして取得
+    }
+    
+    //　Categoryに対するリレーション
+    
+    // 「1対多」の関係なので単数系に
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 }
